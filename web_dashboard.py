1d68c7fb9ee63769e65aab8a19626f90c2301aaa
troujw-cell <troujw@gmail.com>
2026-04-19 17:10:33 -0400

    Update web_dashboard.py

--- web_dashboard.py
+++ web_dashboard.py
@@ -129,5 +129,5 @@ def dashboard():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
